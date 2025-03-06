b828dfafa976707122ca5aaa542fbeb61368ab30
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-03-06 09:46:25 +0800

    Create Learn_D2.mm

---
+++ Learn_D2.mm
@@ -0,0 +1,119 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Learn D2" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1741223977202" LINK="https://d2lang.com/"><hook NAME="MapStyle" background="#f5f5dcff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sepia_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.5 pt;" ENDINCLINATION="81.75 pt;19.875 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="Get D2 and Make it Work" POSITION="bottom_or_right" ID="ID_87807514" CREATED="1741223714180" MODIFIED="1741223739302" LINK="https://d2lang.com/releases/intro/">
+<node TEXT="Download 0.6.9" ID="ID_1960639021" CREATED="1741223775977" MODIFIED="1741223786566" LINK="https://github.com/terrastruct/d2/releases/tag/v0.6.9"/>
+<node TEXT="Configure in VS Code" ID="ID_808734533" CREATED="1741224082600" MODIFIED="1741224092082"/>
+</node>
+<node TEXT="D2 Introduction" POSITION="bottom_or_right" ID="ID_1282326831" CREATED="1741223699726" MODIFIED="1741223848042">
+<node TEXT="What is D2, Make First Diagramming" ID="ID_1188716131" CREATED="1741224098568" MODIFIED="1741224112571"/>
+<node TEXT="Dev Tool vs. Design Tool" ID="ID_451255535" CREATED="1741224232962" MODIFIED="1741224239021"/>
+<node TEXT="Design Decisions" ID="ID_1840821093" CREATED="1741224239652" MODIFIED="1741224243484">
+<node TEXT="Readability &gt; prototyping speed" ID="ID_779630559" CREATED="1741224811229" MODIFIED="1741224821768"/>
+<node TEXT="Warnings &gt; errors" ID="ID_1767423046" CREATED="1741224824765" MODIFIED="1741224830291"/>
+<node TEXT="Good defaults" ID="ID_582122881" CREATED="1741224830451" MODIFIED="1741224832775"/>
+<node TEXT="Optimized for desktop and servers" ID="ID_1414464777" CREATED="1741224832924" MODIFIED="1741224838199"/>
+<node TEXT="Singular use case: documenting software" ID="ID_966401957" CREATED="1741224838341" MODIFIED="1741224847767"/>
+<node TEXT="Design the system, not the diagram" ID="ID_1461781078" CREATED="1741224848308" MODIFIED="1741224857902"/>
+<node TEXT="Whiteboard-fit: diagram vs. graph" ID="ID_1635343962" CREATED="1741224860140" MODIFIED="1741224873264"/>
+</node>
+<node TEXT="Getting help &amp; community" ID="ID_49080626" CREATED="1741224795956" MODIFIED="1741224804425"/>
+</node>
+<node TEXT="Getting Started" POSITION="bottom_or_right" ID="ID_112859093" CREATED="1741223848494" MODIFIED="1741223851064"/>
+<node TEXT="Special Objects" POSITION="bottom_or_right" ID="ID_1509624336" CREATED="1741223851815" MODIFIED="1741223855713"/>
+<node TEXT="Customization" POSITION="bottom_or_right" ID="ID_440793693" CREATED="1741223856231" MODIFIED="1741223859475"/>
+<node TEXT="Layouts" POSITION="bottom_or_right" ID="ID_411853557" CREATED="1741223860081" MODIFIED="1741223861835"/>
+<node TEXT="In Depth in D2" POSITION="bottom_or_right" ID="ID_1713907036" CREATED="1741223862152" MODIFIED="1741223866297"/>
+<node TEXT="Composition" POSITION="bottom_or_right" ID="ID_1745315905" CREATED="1741223890561" MODIFIED="1741223899700"/>
+<node TEXT="Imports" POSITION="bottom_or_right" ID="ID_776475321" CREATED="1741223900233" MODIFIED="1741223904098"/>
+<node TEXT="Extensions" POSITION="bottom_or_right" ID="ID_1928134336" CREATED="1741223904602" MODIFIED="1741223906454"/>
+<node TEXT="API" POSITION="bottom_or_right" ID="ID_484569659" CREATED="1741223906698" MODIFIED="1741224007421">
+<node TEXT="D2 Oracle" ID="ID_185555474" CREATED="1741223937808" MODIFIED="1741223941246"/>
+</node>
+<node TEXT="More on D2" POSITION="bottom_or_right" ID="ID_1535800956" CREATED="1741224007969" MODIFIED="1741224011322">
+<node TEXT="CLI Manual" ID="ID_1431270617" CREATED="1741223941408" MODIFIED="1741223946964"/>
+<node TEXT="Exports" ID="ID_1589845634" CREATED="1741224029387" MODIFIED="1741224031844"/>
+<node TEXT="Cheat Sheet" ID="ID_1358330697" CREATED="1741224032263" MODIFIED="1741224036117"/>
+</node>
+</node>
+</map>
