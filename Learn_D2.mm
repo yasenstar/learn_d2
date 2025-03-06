56bf6a747393b52dc8b9c1608087419952b6616f
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-03-06 23:42:31 +0800

    add 005

--- Learn_D2.mm
+++ Learn_D2.mm
@@ -1,7 +1,7 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="Learn D2" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1741223977202" LINK="https://d2lang.com/"><hook NAME="MapStyle" background="#f5f5dcff" zoom="1.3310001">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sepia_template.mm" show_tags="UNDER_NODES"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sepia_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES"/>
     <tags category_separator="::"/>
 
 <map_styles>
@@ -9,7 +9,7 @@
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.5 pt;" ENDINCLINATION="81.75 pt;19.875 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.125 pt;" ENDINCLINATION="81.75 pt;19.875 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
@@ -98,7 +98,7 @@
 </node>
 <node TEXT="Roadmap" ID="ID_1873591371" CREATED="1741224905555" MODIFIED="1741224909957"/>
 </node>
-<node TEXT="02 Getting Started" POSITION="bottom_or_right" ID="ID_112859093" CREATED="1741223848494" MODIFIED="1741228121238">
+<node TEXT="02 Getting Started" FOLDED="true" POSITION="bottom_or_right" ID="ID_112859093" CREATED="1741223848494" MODIFIED="1741228121238">
 <node TEXT="Get D2 and Make it Work" FOLDED="true" POSITION="bottom_or_right" ID="ID_87807514" CREATED="1741223714180" MODIFIED="1741223739302" LINK="https://d2lang.com/releases/intro/">
 <node TEXT="Methods of Install" ID="ID_806054929" CREATED="1741225123267" MODIFIED="1741225132714" LINK="https://github.com/terrastruct/d2/blob/master/docs/INSTALL.md"/>
 <node TEXT="Download 0.6.9" ID="ID_1960639021" CREATED="1741223775977" MODIFIED="1741223786566" LINK="https://github.com/terrastruct/d2/releases/tag/v0.6.9"/>
